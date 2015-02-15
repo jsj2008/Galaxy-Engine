6cd3ba6fa15f00433f6b7e1122b55fdf8dc4def1
qnope <antoine.morrier@telecom-sudparis.eu>
2015-02-15 17:07:51 +0100

    Comments

--- main.cpp
+++ main.cpp
@@ -10,26 +10,27 @@ int main(int argc, char *argv[])
 {
     try
     {
-        Device device(800, 600);
+        Device device(800, 600); // Create a Device
 
+        // Capture and hide cursor
         device.mouse()->captureCursor(true);
         device.mouse()->showCursor(false);
 
-        SceneManager sceneManager;
+        SceneManager sceneManager; // Create a Scene Manager
 
-        sceneManager.createCameraFPS(vec3(0, 500, 0.1), 300, 0.5);
+        sceneManager.createCameraFPS(vec3(0, 500, 0.1), 300, 0.5); // Create a FPS Camera
 
-        shared_ptr<Node> rootNode = sceneManager.getRootNode();
+        shared_ptr<Node> rootNode = sceneManager.getRootNode(); // Get the Root Node
 
-        shared_ptr<ModelNode> model = addModel(rootNode, "models/OBJ/crytek-sponza/sponza.obj");
+        shared_ptr<ModelNode> model = addModel(rootNode, "models/OBJ/crytek-sponza/sponza.obj"); // Add a Model Sponza Atrium
 
         while(device.run())
         {
-            device.begin();
+            device.begin(); // Clear Window
 
-            sceneManager.render();
+            sceneManager.render(); // Render Scene
 
-            device.end();
+            device.end(); // Swap Buffer
         }
 
     }
